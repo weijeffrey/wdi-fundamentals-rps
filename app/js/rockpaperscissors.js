1a416ad5cc3f83e823113a52783282a934b79462
weijefffrey <jeffreycswei@gmail.com>
2015-06-16 15:28:08 +0800

    declared var move

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,14 +25,16 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return var move = x;
+	x = x||getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return var move = y;
+	y = y||randomPlay()
 }
 
 function getWinner(playerMove,computerMove) {
